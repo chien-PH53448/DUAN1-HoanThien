4bf599c44a24f4ee192e929b4803b1cfa73cf849
vuminhchien <chienvmph53448@gmail.com>
2024-12-04 10:03:15 +0700

    lan 2

--- env.php
+++ env.php
@@ -1,5 +1,5 @@
 <?php
 
 const ROOT_DIR = __DIR__ . "/";
-const ROOT_URL = "http://localhost/N6/";
-const ADMIN_URL = "http://localhost/N6/admin/";
+const ROOT_URL = "http://localhost/demo/";
+const ADMIN_URL = "http://localhost/demo/admin/";
